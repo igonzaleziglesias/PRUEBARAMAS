96b7a451bccd8e32033775b4acc229da8fcd5689
aguinaldogonzalez <aguinaldogonzalez@nautilus4.danielcastelao.org>
2018-11-13 09:57:47 +0100

    Commit A de ramaAngel

---
+++ ramas/ClaseAngel.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramas;
+
+/**
+ *
+ * @author aguinaldogonzalez
+ */
+public class ClaseAngel {
+    //Nueva clase en rama Angel
+    
+}
